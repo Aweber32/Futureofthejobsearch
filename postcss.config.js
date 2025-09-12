ad282580f8c46056d97165cf06835fbfb5fab78c
Alex Weber <alexweber12899@gmail.com>
2025-09-12 18:38:16 -0400

    deleted files

---
+++ postcss.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  plugins: {
+    // Use the official Tailwind PostCSS plugin wrapper (installed as @tailwindcss/postcss)
+    '@tailwindcss/postcss': {},
+    autoprefixer: {}
+  }
+};
