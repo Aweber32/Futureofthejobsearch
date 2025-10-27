241bc51e169dff8a24e55a8a1a9f176040005573
Alex Weber <alexweber12899@gmail.com>
2025-10-27 14:21:18 -0400

    adding more logging for debugging

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,10 @@ export default function MyApp({ Component, pageProps }) {
   useEffect(() => {
     // Import Bootstrap JS only on client side
     import('bootstrap/dist/js/bootstrap.bundle.min.js');
+    
+    // Diagnostic: Log environment variables
+    console.log('[_app.js] NEXT_PUBLIC_API_URL:', process.env.NEXT_PUBLIC_API_URL);
+    console.log('[_app.js] NODE_ENV:', process.env.NODE_ENV);
   }, []);
 
   return <Component {...pageProps} />;
